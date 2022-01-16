58030b533fa28ec28b4f08c637738711d6f0c125
vibin.chakravarthy <vibin.chakravarthy@zohocorp.com>
2022-01-16 20:47:25 +0530

    changes

--- src/Components/Vendors/ListVendors.js
+++ src/Components/Vendors/ListVendors.js
@@ -1,10 +1,21 @@
 import { Button, Flex } from "@aws-amplify/ui-react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { DataGrid } from '@mui/x-data-grid';
+import { listVendors } from "../../graphql/queries";
+import { API, graphqlOperation } from "aws-amplify";
 
 export default function ListVendors(props) {
 
-    console.log(props.vendors);
+    const [vendors, SetVendors] = useState([]);
+    const [loading, SetLoading] = useState(true);
+
+    useEffect(() => {
+        API.graphql(graphqlOperation(listVendors))
+        .then(response => {
+            SetVendors(response.data.listVendors.items)
+            SetLoading(false);
+        });
+    }, []);
 
     const columns = [
         {
@@ -26,11 +37,13 @@ export default function ListVendors(props) {
                 </Flex>
                 <Flex>
                     <DataGrid
-                        rows={props.vendors} 
+                        loading={loading}
+                        rows={vendors} 
                         columns={columns}
                         pageSize={5}
                         rowsPerPageOptions={[5]}
                         autoHeight={true}
+                        columnBuffer={2}
                     />
                 </Flex>
             </Flex>
--- src/Components/Vendors/index.js
+++ src/Components/Vendors/index.js
@@ -1,46 +1,18 @@
-import React, { Component }  from "react";
-import { listVendors } from "../../graphql/queries";
-import { API, graphqlOperation } from "aws-amplify";
+import React, { useState }  from "react";
 import AddVendors from "./AddVendors";
 import ListVendors from "./ListVendors";
 
-class Vendors extends Component {
+export default function Vendors() {
+    
+    const [add, SetAdd] = useState(false);
 
-    state = {
-        add     : false,
-        vendors : []
-    }
-
-    componentDidMount() {
-        // API.graphql(graphqlOperation(listVendors))
-        // .then(response => );
-        this.setState({ vendors : [
+    return (
+        <div style={{padding:"20px"}}>
             {
-                id: 1,
-                name: "Vibin",
-                phone: "1234"
-            },
-            {
-                id: 2,
-                name: "Emperor",
-                phone: "12345678"
+                add ?
+                <AddVendors cancelAdd={() => SetAdd(false)} /> : 
+                <ListVendors addVendor={() => SetAdd(true)} />
             }
-        ] })
-    }
-
-
-
-    render() {
-        return (
-            <div style={{padding:"20px"}}>
-                {
-                    this.state.add ?
-                    <AddVendors cancelAdd={() => this.setState({ add : false})}/> : 
-                    <ListVendors addVendor={() => this.setState({ add : true})} vendors={this.state.vendors}/>
-                }
-            </div>
-        )
-    }
+        </div>
+    )
 }
-
-export default Vendors;
