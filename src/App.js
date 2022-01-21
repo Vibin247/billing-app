9875717eedb7c4d8af27cdfa05e1ea215d5ed9f9
vibin.chakravarthy <vibin.chakravarthy@zohocorp.com>
2022-01-21 18:22:18 +0530

    changes.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import Vendors from "./Components/Vendors";
 import Amplify from "aws-amplify";
 import aws_exports from "./aws-exports";
+import BillForm from "./Components/BillForm";
+import { TabItem, Tabs } from "@aws-amplify/ui-react";
 
 Amplify.configure(aws_exports);
 
 function App() {
   return (
-    <div className="App">
-      <Vendors/>      
+    <div className="app">
+      <div className="header">
+        Dev Prakash Diwan Cots
+      </div>
+      <div className="Wrapper">
+        <Tabs indicatorPosition="top" flex={1}>
+          <TabItem title="Sales">
+            <BillForm/>
+          </TabItem>
+          <TabItem title="Carpenters">
+            <Vendors/>
+          </TabItem>
+        </Tabs>
+      </div>
     </div>
   );
 }
--- src/Components/BillForm.js
+++ src/Components/BillForm.js
@@ -12,15 +12,26 @@ export default function BillForm(props) {
     const saveSale = () => {
         const postData = { name, address, itemDetails, quantity, rate };
         console.log(postData);
+        window.html2pdf(document.getElementById("testPDF")).save()
     }
 
-    return (
-        <Flex direction={"column"} gap={"25px"} padding={"25px"}>
-            <TextField label="Buyer Name" placeholder="Deva Doss"
-             onChange={e => SetName(e.target.value)} value={name}/>
+    var options = {year: 'numeric', month: 'long', day: 'numeric' };
+    const today  = new Date().toLocaleDateString("en-IN", options);
 
-            <TextField label="Buyer address" placeholder="Street, Area, Pincode"  isMultiline={true} 
-             onChange={e => SetAddress(e.target.value)} value={address} rows={3}/>
+    return (
+        <Flex direction={"column"} gap={"25px"} padding={"25px"} id="testPDF">
+
+            <Flex direction={"row"} justifyContent={"flex-start"}>
+                <TextField label="Date" disabled={true} value={today}/>
+            </Flex>
+
+            <Flex direction={"row"} justifyContent={"space-evely"}>
+                <TextField label="Buyer Name" placeholder="Deva Doss" className="flex-1"
+                onChange={e => SetName(e.target.value)} value={name}/>
+
+                <TextField label="Buyer address" placeholder="Street, Area, Pincode"  isMultiline={true} className="flex-1"
+                onChange={e => SetAddress(e.target.value)} value={address} rows={3}/>
+            </Flex>
 
             <Flex direction={"row"} justifyContent={"space-between"}>
                 <TextField label="Item details" placeholder="Product type" className="flex-4"
@@ -43,9 +54,11 @@ export default function BillForm(props) {
                 </Flex>
              </Flex>
 
-             <Button onClick={saveSale}>
-                Save Sale
-             </Button>
+            <Flex direction={"row"} justifyContent={"flex-end"}>
+                <Button onClick={saveSale}>
+                    Generate Bill
+                </Button>
+            </Flex>
         </Flex>
     )
 }
